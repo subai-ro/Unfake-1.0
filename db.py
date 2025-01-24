1708e38c7019a762c3a363323bfc819bb93d5a89
subai-ro <inikita.silivestrov@gmail.com>
2025-01-24 20:48:36 +0100

    Fix database schema

--- db.py
+++ db.py
@@ -12,7 +12,8 @@ import pickle
 _model_pipeline = None
 
 def get_connection():
-    conn = sqlite3.connect(r"D:\Unfake project\unfake.db")
+    db_path = os.path.join(os.getcwd(), "unfake.db")  # Ensures DB is in the working directory
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
 
